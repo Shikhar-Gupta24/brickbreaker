459eb826691060d125dd5af840e08585e2c22bbc
goop <80974447+Shikhar-Gupta24@users.noreply.github.com>
2023-07-10 22:44:25 -0400

    Add files via upload

--- GUIDriver.java
+++ GUIDriver.java
@@ -1,9 +1,8 @@
 public class GUIDriver {
     public static void main(String[] args){//Starts the program panel
         DisplayWindow d = new DisplayWindow("Test");
-        GUIPanel p = new GUIPanel(500, 700);
+        GUIpanel p = new GUIpanel(500, 700);
         d.add(p);
         d.showFrame();
-        }
+    }
 }
-
